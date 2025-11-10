8ff08cdf82b18757252b07ecb4eead838af8796e
Rob-C-89 <robbiecornell@gmail.com>
2025-11-10 13:04:42 +0000

    Add sales data to google worksheet

--- run.py
+++ run.py
@@ -52,4 +52,16 @@ def validate_data(values):
     return True
 
 
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided.
+    """
+    print("Updating sales worksheet...\n")
+    sales_datasheet = SHEET.worksheet("sales")
+    sales_datasheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
+
+
 data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
